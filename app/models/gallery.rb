435655e27a9eaee398120e035185050bc65aee01
Mateusz Król <kroolikk@gmail.com>
2012-06-24 21:32:06 +0200

    rozwiazanie problemu z labelem galerii

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,7 @@
 class Gallery < ActiveRecord::Base
+  
+  attr_protected :id
+
   has_many :photos, :dependent => :destroy
   accepts_nested_attributes_for :photos, :reject_if => lambda { |a| a[:photo].blank? }, :allow_destroy => true
 
