d88677a48f7f4096ca50079dd47bc24aa6f87012
Mateusz Król <kroolikk@gmail.com>
2012-07-04 23:23:19 +0200

    poprawki w panelu admina

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,26 @@
 module AdminHelper
+  
+  def pages_label(label)
+    case label
+    when "about_stable" then "Stajnia - O stajni"
+    when "offer_stable" then "Stajnia - Oferta"
+    when "about_pension" then "Pensjonat - O pensjonacie"
+    when "rooms_pension" then "Pensjonat - Pokoje" 
+    when "attractions_pension" then "Pensjonat - Atrakcje" 
+    when "about_station" then "Stacja - O stacji"
+    when "offer_station" then "Stacja - Oferta"
+    else "Brak etykiety"
+    end
+  end
+
+  
+  def main_label(label)
+    case label
+    when "stable" then "Stajnia"
+    when "pension" then "Pensionat"
+    when "station" then "Stacja"
+    else "Brak etykiety"
+    end
+  end
+
 end
